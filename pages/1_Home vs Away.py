7930ee0011ee66d125cb41d8c2e177e0acdbb101
axelbol <axel_lp_alvarado@hotmail.com>
2025-05-21 20:15:27 -0400

    changed order boxes home vs away

--- pages/1_Home vs Away.py
+++ pages/1_Home vs Away.py
@@ -110,7 +110,6 @@ def get_team_stats(shots):
 def display_team_metrics(team_stats):
     """Display team metrics in a 2x2 grid."""
     col1, col2 = st.columns(2)
-
     with col1:
         st.metric(
             label="Team with Most Home Shots",
@@ -121,20 +120,20 @@ def display_team_metrics(team_stats):
 
     with col2:
         st.metric(
-            label="Team with Most Away Shots",
-            value=team_stats['away']['most']['team'],
-            delta=f"{int(team_stats['away']['most']['count'])} shots",
-            border=True
-        )
-
-    col3, col4 = st.columns(2)
-
-    with col3:
-        st.metric(
             label="Team with Least Home Shots",
             value=team_stats['home']['least']['team'],
             delta=f"{int(team_stats['home']['least']['count'])} shots",
             delta_color="inverse",
+            border=True
+        )
+
+
+    col3, col4 = st.columns(2)
+    with col3:
+        st.metric(
+            label="Team with Most Away Shots",
+            value=team_stats['away']['most']['team'],
+            delta=f"{int(team_stats['away']['most']['count'])} shots",
             border=True
         )
 
